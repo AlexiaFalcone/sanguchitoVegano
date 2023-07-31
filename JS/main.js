d68c0fe261782aeb766a8c5934babee8484bf454
alexiafalcone <alexiafalcone1995@gmail.com>
2023-07-31 20:20:53 -0300

    carrito de compra 2da entrega

--- JS/main.js
+++ JS/main.js
@@ -8,8 +8,6 @@ let producto = 1200;
 
 stock();
 
-
-
 function stock() {
   const docenas = parseInt(prompt("¿Cuantás docenas desea?"));
   const resultado = docenas * producto;
@@ -51,10 +49,10 @@ function stock() {
       break;
 
     case 3:
-      const recargo = resultado * 10 / 100;
+       const recargo = resultado * 10 / 100;
       const valorTresCuotas = resultado + recargo;
       const valorPorCuota = valorTresCuotas / 3;
-      alert("El monto a abonar es:" + " " + valorTresCuotas + " " + ",en 3 cuotas de:" + " " + valorPorCuota);
+     alert("El monto a abonar es:" + " " + valorTresCuotas + " " + ",en 3 cuotas de:" + " " + valorPorCuota);
       break;
   }
 
