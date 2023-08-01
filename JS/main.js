a3ed7251b8aaccc67d9c506df3bcc0b1715387b1
alexiafalcone <alexiafalcone1995@gmail.com>
2023-08-01 18:21:15 -0300

    agregue metodos de array

--- JS/main.js
+++ JS/main.js
@@ -1,69 +1,89 @@
+// PRODUCTOS DISPONIBLES //
+
+class productosDisponibles {
+  constructor(nombre, precio, descripcion){
+  this.nombre = nombre;
+  this.precio = precio;
+  this.descripcion = descripcion;
+  }
+};
+
+const productos = [
+  new productosDisponibles ("Pan Integral", 850, "Pan integral grande"),
+  new productosDisponibles ("Chipa Vegano", 1000, "Valor por un 250 gr"),
+  new productosDisponibles ("Sanguchitos Caprese", 1600, "Valor por 1/2 docena"),
+];
+
+console.log (productos);
+
+// CARRITO //
+
+let carrito = [];
+
+
+
+// INTERACCION CON EL USUARIO//
+
 let nombre = prompt("Hola, ¿Cuál es tu nombre?");
-alert("Bienvenido" + " " + nombre + " " + "a Sanguchito Vegano.");
+alert("Bienvenido " + nombre + " a Sanguchito Vegano.");
 
-let docenaDeSanguchitos = 2;
+function mostrarProductos (){
 
+const elegirProducto = prompt ("¿Qué desea comprar? \nPan Integral \nChipa Vegano  \nSanguchitos Caprese");
 
-let producto = 1200;
+const productoSeleccionado = productos.find (producto => producto.nombre.toLowerCase() === elegirProducto.toLowerCase());
 
-stock();
+carrito.push(productoSeleccionado);
 
-function stock() {
-  const docenas = parseInt(prompt("¿Cuantás docenas desea?"));
-  const resultado = docenas * producto;
+console.log (carrito);
 
-  if (docenas === 1) {
-    alert("El monto a abonar es" + " " + producto);
-  }
+const seguirComprando = parseInt(prompt("¿Te gustaría seguir comprando? \n 1 Si \n2 No"));
 
-  if (docenas === 2) {
-    alert("El monto a abonar es" + " " + resultado);
-  }
-  if (docenas > 2) {
-    alert(
-      "Lo sentimos solo tenemos" +
-      " " +
-      docenaDeSanguchitos +
-      " " +
-      "docenas en stock"
-    );
-  }
+while (seguirComprando === 1 ){
+  mostrarProductos();
+  return;
+}
+}
+mostrarProductos();
 
-  let opcionesDePago = parseInt(
-    prompt(
-      "Seleccione la forma de pago: \n 1. Pago efectivo, 10% OFF \n  2. Pago con tarjeta 1 pago, sin recargo \n 3. Pago con tarjeta en 3 cuota, 10% de recargo"
-    )
-  );
+const mostrarCarrito = carrito.map((prod) => prod.nombre);
 
-  switch (opcionesDePago) {
-    case 1:
-      const descuento = resultado * 10 / 100;
-      console.log(descuento);
-      const valorEfectivo = resultado - descuento;
-      alert("El monto es:" + " " + valorEfectivo);
-      break;
+console.log (mostrarCarrito);
 
-    case 2:
-      const valorUnPago = resultado;
-      alert("El monto es:" + " " + valorUnPago);
-      break;
+const valorCompra = carrito.reduce ((acumulador, producto) => acumulador + producto.precio, 0);
 
-    case 3:
-       const recargo = resultado * 10 / 100;
-      const valorTresCuotas = resultado + recargo;
-      const valorPorCuota = valorTresCuotas / 3;
-     alert("El monto a abonar es:" + " " + valorTresCuotas + " " + ",en 3 cuotas de:" + " " + valorPorCuota);
-      break;
-  }
-
-  const seguirComprando = parseInt(prompt("¿Te gustaría realizar otra compra? \n 1. Si \n 2. No"));
-
-  while (seguirComprando === 1) {
-    stock();
-    return;
-  }
+const itemsCompra = alert("Los Items seleccionados son: \n" + mostrarCarrito + "\n El valor total es:" + valorCompra);
 
 
+
+// OPCIONES DE PAGO//
+
+
+let opcionesDePago = parseInt(
+  prompt(
+    "Seleccione la forma de pago: \n 1. Pago efectivo, 10% OFF \n  2. Pago con tarjeta 1 pago, sin recargo \n 3. Pago con tarjeta en 3 cuota, 10% de recargo"
+  )
+);
+
+switch (opcionesDePago) {
+  case 1:
+    const descuento = valorCompra * 10 / 100;
+    console.log(descuento);
+    const valorEfectivo = valorCompra - descuento;
+    alert("El monto es:" + " " + valorEfectivo);
+    break;
+
+  case 2:
+    const valorUnPago = valorCompra;
+    alert("El monto es:" + " " + valorUnPago);
+    break;
+
+case 3:
+     const recargo = valorCompra * 10 / 100;
+    const valorTresCuotas = valorCompra + recargo;
+    const valorPorCuota = valorTresCuotas / 3;
+  alert("El monto a abonar es:" + " " + valorTresCuotas + " " + ",en 3 cuotas de:" + " " + parseInt(valorPorCuota));
+    break;
 }
 
 alert("¡Muchas gracias por tu compra!");
