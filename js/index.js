a746d5cf53da19a56088f7f015a0ef97db47c31f
alexiafalcone <alexiafalcone1995@gmail.com>
2023-08-27 16:27:08 -0300

    cambios en las funciones

--- js/index.js
+++ js/index.js
@@ -4,7 +4,8 @@ const productos = [
         nombre: "Pan integral",
         precio: 850,
         stock: 10,
-        img: "../img/panIntegral.jpeg"
+        img: "../img/panIntegral.jpeg",
+        cantidad: 0
     },
 
     {
@@ -12,7 +13,8 @@ const productos = [
         nombre: "Chipa Vegano",
         precio: 1000,
         stock: 15,
-        img: "../img/chipacito.jpeg"
+        img: "../img/chipacito.jpeg",
+        cantidad: 0
     },
 
     {
@@ -20,43 +22,82 @@ const productos = [
         nombre: "Sanguchito Caprese",
         precio: 1600,
         stock: 10,
-        img: "../img/sanguchito.jpeg"
+        img: "../img/sanguchito.jpeg",
+        cantidad: 0
     },
 ];
 
 let carrito = [];
 
+JSON.parse(localStorage.getItem("carrito"));
+
 const prodBox = document.querySelector("#listaProductos");
-//const agregarProducto = document.querySelectorAll("#agregar");
-//console.log(agregarProducto)
-
-
-
-
 
 const mostrarCardHtml = () => {
-    productos.forEach((producto) =>{
-        const {id, nombre, precio, img} = producto
-        prodBox.innerHTML += `<div>
-        <img src=${img}>
+    prodBox.innerHTML = "";
+
+    productos.forEach((producto) => {
+        const { id, nombre, precio, img } = producto
+        const cardProd = document.createElement("div");
+        cardProd.innerHTML = `
+        <img class="imgProd" src=${img}>
         <p>Nombre:${nombre}</p>
-        <p>Precio:${precio}</p>
-        </div>`;
+        <p>Precio:${precio}</p>`;
+        prodBox.appendChild(cardProd)
 
         const button = document.createElement("button")
-        button.innerHTML = "Agregar al carrito"
+        button.innerHTML = "Agregar al carrito";
         prodBox.appendChild(button)
 
-        button.onclick =(e)=> agregarAlCarrito(e);
+        button.onclick = () => agregarAlCarrito(id);
 
     })
 };
 
-const agregarAlCarrito=(e)=>{
-    console.log(e);
-    
+
+const agregarAlCarrito = (id, cantidad) => {
+
+    const productoSeleccionado = productos.find(prod => prod.id === id);
+    productoSeleccionado.cantidad += 1
+
+    const existe = carrito.find(prod => prod.id == productoSeleccionado.id);
+    if (existe) {
+        cantidad++;
+        carrito.push(productoSeleccionado);
+        localStorage.setItem("carrito", JSON.stringify(carrito));
+
+    } else {
+        productoSeleccionado.cantidad = 1;
+        carrito.push(productoSeleccionado);
+        localStorage.setItem("carrito", JSON.stringify(carrito));
+        console.log(productoSeleccionado)
+    }
+
+
 }
 
+const carritoItem = document.querySelector("#carrito");
 
+const mostrarCarrito = () => {
+    carritoItem.innerHTML = " ";
+    carrito = JSON.parse(localStorage.getItem("carrito"));
+    if (carrito !== null) {
+        carrito.forEach((producto) => {
+            const { nombre, precio, cantidad } = producto
+            let contenedorCarrito = document.createElement("div");
+            contenedorCarrito.innerHTML = `
+                <p>Nombre:${nombre}</p>
+                <p>Precio:${precio}</p>
+                <p>Cantidad: ${cantidad}</p>
+                <p>Valor: ${precio * cantidad}</p>
+            `
+            carritoItem.appendChild(contenedorCarrito);
+
+        })
+    }
+
+}
 
 mostrarCardHtml();
+mostrarCarrito();
+
